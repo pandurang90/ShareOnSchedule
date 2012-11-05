5d3ecdf7471ae76baafad496820c4e3f854b6586
pandurang <pandurang.plw@gmail.com>
2012-11-05 13:54:25 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareOnSchedule::Application.config.secret_token = '9d3f893b2eee0575fffd47ab1f01d96863e6dc45583f3a9ea08e743642b924e893f3a457abd03c1a2e76ba081126196c5d7938eca3e72bd9ccbdfa12250339b2'
