6db9e349156d1c9526ff4b4947539eaf1c9703a3
pandurang <pandurang.plw@gmail.com>
2012-11-05 14:00:48 +0530

    initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ShareOnSchedule::Application.config.secret_token = '9d3f893b2eee0575fffd47ab1f01d96863e6dc45583f3a9ea08e743642b924e893f3a457abd03c1a2e76ba081126196c5d7938eca3e72bd9ccbdfa12250339b2'
+ShareOnSchedule::Application.config.secret_token = '0832750358bb4ba6907a2643c9b698871bb841e38f3a085059f95afcfdeab93bc5bfecb23a9cb08d174a5fc51e9a47ee63101179c55e318af4939ada9fbf8132'
