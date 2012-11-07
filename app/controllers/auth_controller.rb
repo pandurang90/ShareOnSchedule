23113182b451e1fc54c4efb56ba99918244c92be
nishank85 <nishank@idyllic-software.com>
2012-11-07 12:13:28 +0530

    Added functionality for linkedin

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,25 +3,24 @@ require 'linkedin'
 class AuthController < ApplicationController
   def index
     # get your api keys at https://www.linkedin.com/secure/developer
-    client = LinkedIn::Client.new("u75dvcqku00k", "KH6gnEKHs71Ph8Vx")
+    client = LinkedIn::Client.new("YOUR_TOKEN", "YOUR_SECRET")
     request_token = client.request_token(:oauth_callback => 
                                   "http://#{request.host_with_port}/auth/callback")
-    session[:rtoken] = request_token.token
-    session[:rsecret] = request_token.secret
+    session[:rtoken] = request_token.token # request token
+    session[:rsecret] = request_token.secret # request secret
     redirect_to client.request_token.authorize_url
   end
 
   def callback
-    client = LinkedIn::Client.new("u75dvcqku00k", "KH6gnEKHs71Ph8Vx")
+    client = LinkedIn::Client.new("YOUR_TOKEN", "YOUR_SECRET")
     if session[:atoken].nil?
       pin = params[:oauth_verifier]
       atoken, asecret = client.authorize_from_request(session[:rtoken], session[:rsecret], pin)
-      session[:atoken] = atoken
-      session[:asecret] = asecret
+      session[:atoken] = atoken # authentication token
+      session[:asecret] = asecret # authentication secret
     else
       client.authorize_from_access(session[:atoken], session[:asecret])
     end
-    @profile = client.profile
-    #@connections = client.connections
+    @profile = client.profile(:fields => ['first_name','last_name'])
   end
 end
