85a8c2f61733a51c78e167a564f20d5d6b5705ff
nishank85 <nishank@idyllic-software.com>
2012-11-07 12:37:48 +0530

    Fetch first & last name from linkedin & display in view

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,7 @@ require 'linkedin'
 class AuthController < ApplicationController
   def index
     # get your api keys at https://www.linkedin.com/secure/developer
-    client = LinkedIn::Client.new("YOUR_TOKEN", "YOUR_SECRET")
+    client = LinkedIn::Client.new("Your_API_KEY", "Your_SECRET")
     request_token = client.request_token(:oauth_callback => 
                                   "http://#{request.host_with_port}/auth/callback")
     session[:rtoken] = request_token.token # request token
@@ -12,7 +12,7 @@ class AuthController < ApplicationController
   end
 
   def callback
-    client = LinkedIn::Client.new("YOUR_TOKEN", "YOUR_SECRET")
+    client = LinkedIn::Client.new("Your_API_KEY", "Your_SECRET")
     if session[:atoken].nil?
       pin = params[:oauth_verifier]
       atoken, asecret = client.authorize_from_request(session[:rtoken], session[:rsecret], pin)
@@ -21,6 +21,7 @@ class AuthController < ApplicationController
     else
       client.authorize_from_access(session[:atoken], session[:asecret])
     end
-    @profile = client.profile(:fields => ['first_name','last_name'])
+    @profile = client.profile(:fields => [:first_name]).first_name
+    @profiles = client.profile(:fields => [:last_name]).last_name  
   end
 end
