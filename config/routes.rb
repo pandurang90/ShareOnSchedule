eeb0b6cd83c2ad76b7be06ea9f84f6d3f2b43076
pandurang <pandurang.plw@gmail.com>
2012-11-08 16:40:26 +0530

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 ShareOnSchedule::Application.routes.draw do
+  root :to => 'welcome#index'
+  resources :tweets
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}    
 end
