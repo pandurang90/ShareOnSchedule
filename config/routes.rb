0201e1db4dfe82033f6ebd2236917edb91f0680c
deepali <singhdeepalit@gmail.com>
2012-11-08 16:49:56 +0530

    routes issue resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 ShareOnSchedule::Application.routes.draw do
+ root :to => 'welcome#index'
+  resources :tweets
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  #match '/auth/:provider/callback' => 'auth#callback'
 end
