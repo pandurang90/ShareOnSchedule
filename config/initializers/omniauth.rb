d7858a26bf3404f650ab798adabf4364ee02e1ca
Nishank <nishank@idyllic-software.com>
2012-11-08 12:08:17 +0530

    Update config/initializers/omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :linkedin, "3k4b76isgyvi", "vRAIHUmoyd4wDkJl", :scope => 'r_fullprofile r_emailaddress rw_nus r_network', :fields => ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location", "connections"]
+  provider :linkedin, "key", "secret", :scope => 'r_fullprofile r_emailaddress rw_nus r_network', :fields => ["id", "email-address", "first-name", "last-name", "headline", "industry", "picture-url", "public-profile-url", "location", "connections"]
 end
