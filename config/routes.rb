a85c6ef36b1b7c8b540171efc70f5a647c25b243
Nishank <nishank@idyllic-software.com>
2012-11-08 14:58:09 +0530

    Update config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,2 @@
 ShareOnSchedule::Application.routes.draw do
-  root :to => 'welcome#index'
-  resources :tweets
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
-  match '/auth/:provider/callback' => 'auth#callback'
-  match '/auth/callback' => 'auth#callback'
 end
