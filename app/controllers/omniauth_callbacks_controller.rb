da2ccb956b813cb1764aa336ed10a059300faf1e
deepali <singhdeepalit@gmail.com>
2012-11-09 18:33:12 +0530

    Small fix

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -11,7 +11,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 				sign_in_and_redirect current_user
 			end
 		else
-<<<<<<< HEAD
 			# IF account is found
 			if(@account=Account.find_by_provider_and_uid(auth['provider'], auth['uid']))
 				#Add set current user is account user
@@ -38,19 +37,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 																				 :provider => auth['provider'], 
 																				 :oauth_token => auth['credentials']['token'])
  				end	
-				
-=======
-			if(@account = Account.find_by_provider_and_uid(auth['provider'], auth['uid']))
-				current_user = @account.user
-				sign_in_and_redirect current_user
-			else
-				@user = User.create(:password => "asdf123", :password_confirmation => "asdf123", :email => auth['info']['email']) if auth['provider'] == "facebook"
-				@user = User.create(:password => "asdf123", :password_confirmation => "asdf123", :email => auth['info']['email']) if auth['provider'] == "twitter"
-				@user.save! 
-				@account = @user.accounts.create(:username => auth['info']['nickname'], :uid => auth['uid'], :provider => auth['provider'])
-				current_user = @account.user
-				sign_in_and_redirect current_user
->>>>>>> f9a5770f5e1eb0caa83d54cd8c28472dee4800fc
 			end
 			current_user=@account.user
 			sign_in_and_redirect current_user
@@ -79,17 +65,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	 			sign_in_and_redirect current_user
 	 		end
 	 	end
-	 	#client = LinkedIn::Client.new("3k4b76isgyvi", "vRAIHUmoyd4wDkJl")
-	 	#request_token = client.request_token(:oauth_callback => "https://www.linkedin.com/uas/oauth/authorize?omniauth_callbacks")
-#https://www.linkedin.com/uas/oauth/authorize?oauth_token=5e548c6e-5726-48b
-	 	#pin = params[:oauth_verifier]
-	 	#client.authorize_from_request("3k4b76isgyvi","vRAIHUmoyd4wDkJl",pin)
-	 	#@profile = client.profile
-    #@connections = client.connections
-#binding.pry
-#response = client.add_share(:comment => 'Hello !')
-#puts "#{response}"
-#sleep(5.0)
 	end
 
 alias_method :twitter, :all # method over-riding
