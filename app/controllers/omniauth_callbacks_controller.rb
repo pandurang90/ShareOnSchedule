1ec6ab548848f7cf4a5ca887c505a61de667c0bb
nishank85 <nishank@idyllic-software.com>
2012-11-09 15:19:30 +0530

    Modeified omniauth_callback controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -25,8 +25,30 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		end
 	end
 
+  def linkedin
+	 	auth = request.env["omniauth.auth"]
+	 	if(current_user)
+	 		if(Account.find_by_provider_and_uid(auth['provider'], auth['uid']))
+	 			sign_in_and_redirect current_user
+	 		else
+	 			current_user.accounts.create(:username => auth['username'], :uid => auth['uid'], :provider => auth['provider'])
+	 			sign_in_and_redirect current_user
+	 		end
+	 	else
+	 		if(@account = Account.find_by_provider_and_uid(auth['provider'], auth['uid']))
+	 			current_user = @account.user
+	 			sign_in_and_redirect current_user
+	 		else
+	 			@user = User.create(:password => "asdf123", :password_confirmation => "asdf123", :email => auth['info']['email'])
+	 			@user.save! 
+	 			@account = @user.accounts.create(:username => auth['info']['first_name'], :uid => auth['uid'], :provider => auth['provider'])
+				current_user = @account.user
+	 			sign_in_and_redirect current_user
+	 		end
+	 	end
+	end
+
 alias_method :twitter, :all
 alias_method :facebook, :all
-alias_method :linkedin, :all
 
 end
