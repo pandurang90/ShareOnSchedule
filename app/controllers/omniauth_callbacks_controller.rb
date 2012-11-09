f9a5770f5e1eb0caa83d54cd8c28472dee4800fc
nishank85 <nishank@idyllic-software.com>
2012-11-09 16:07:48 +0530

    done some changes

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -16,7 +16,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 			else
 				@user = User.create(:password => "asdf123", :password_confirmation => "asdf123", :email => auth['info']['email']) if auth['provider'] == "facebook"
 				@user = User.create(:password => "asdf123", :password_confirmation => "asdf123", :email => auth['info']['email']) if auth['provider'] == "twitter"
-				@user = User.create(:password => "asdf123", :password_confirmation => "asdf123", :email => auth['info']['email']) if auth['provider'] == "linkedin"
 				@user.save! 
 				@account = @user.accounts.create(:username => auth['info']['nickname'], :uid => auth['uid'], :provider => auth['provider'])
 				current_user = @account.user
@@ -46,9 +45,20 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	 			sign_in_and_redirect current_user
 	 		end
 	 	end
+	 	#client = LinkedIn::Client.new("3k4b76isgyvi", "vRAIHUmoyd4wDkJl")
+	 	#request_token = client.request_token(:oauth_callback => "https://www.linkedin.com/uas/oauth/authorize?omniauth_callbacks")
+#https://www.linkedin.com/uas/oauth/authorize?oauth_token=5e548c6e-5726-48b
+	 	#pin = params[:oauth_verifier]
+	 	#client.authorize_from_request("3k4b76isgyvi","vRAIHUmoyd4wDkJl",pin)
+	 	#@profile = client.profile
+    #@connections = client.connections
+#binding.pry
+#response = client.add_share(:comment => 'Hello !')
+#puts "#{response}"
+#sleep(5.0)
 	end
 
-alias_method :twitter, :all
-alias_method :facebook, :all
+alias_method :twitter, :all # method over-riding
+alias_method :facebook, :all # method over-riding
 
 end
