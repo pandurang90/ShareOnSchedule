5721ec20f8070c772c3aae1d94db4f67aac23213
nishank85 <nishank@idyllic-software.com>
2012-11-09 12:19:41 +0530

    removed password from database.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable,
-         :token_authenticatable, :confirmable,
-  			 :lockable
+         :recoverable, :rememberable, :trackable, :validatable
+         
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   has_many :accounts
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -90,20 +90,20 @@ Devise.setup do |config|
   # able to access the website for two days without confirming his account,
   # access will be blocked just in the third day. Default is 0.days, meaning
   # the user cannot access the website without confirming his account.
-  config.allow_unconfirmed_access_for = 1.day
+  #config.allow_unconfirmed_access_for = 1.day
 
   # If true, requires any email changes to be confirmed (exactly the same way as
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed new email is stored in
   # unconfirmed email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  #config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [ :email ]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  config.remember_for = 1.day
+  #config.remember_for = 1.day
 
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
@@ -168,11 +168,11 @@ Devise.setup do |config|
   # :authlogic_sha512 (then you should set stretches above to 20 for default behavior)
   # and :restful_authentication_sha1 (then you should set stretches to 10, and copy
   # REST_AUTH_SITE_KEY to pepper)
-  config.encryptor = :sha512
+  #config.encryptor = :sha512
 
   # ==> Configuration for :token_authenticatable
   # Defines name of the authentication token params key
-  config.token_authentication_key = :auth_token
+  #config.token_authentication_key = :auth_token
 
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
