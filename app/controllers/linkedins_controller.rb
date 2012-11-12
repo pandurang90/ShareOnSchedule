d9cbc8fa27aeeae44e43ef62dbf922a6d8f0ab46
nishank85 <nishank@idyllic-software.com>
2012-11-12 20:03:34 +0530

    Done some minor changes

--- app/controllers/linkedins_controller.rb
+++ app/controllers/linkedins_controller.rb
@@ -13,11 +13,6 @@ class LinkedinsController < ApplicationController
     @linkedin = current_user.linkedins.new(params[:linkedin])
     respond_to do |format|
       if @linkedin.save
-        # linkedin_provider = current_user.accounts.find_by_provider('linkedin')
-        # token = linkedin_provider.oauth_token
-        # secret = linkedin_provider.oauth_token_secret
-        # verifier = linkedin_provider.oauth_verifier
-        # LinkedinWorker.perform_at(token,secret,verifier,@linkedin.share)
         @account = current_user.accounts.where(:provider => "linkedin").first
         time = Linkedin.where(:id => @linkedin.id).select("TIME_TO_SEC(TIME_TO_SEC(post_time)-TIME_TO_SEC(NOW())) as second").first.second
         LinkedinWorker.perform_at(time.seconds.from_now,@account.oauth_token,@account.oauth_token_secret,@account.oauth_verifier,@linkedin.share)
@@ -53,14 +48,9 @@ class LinkedinsController < ApplicationController
     @linkedin = current_user.linkedins.find(params[:id])
     respond_to do |format|
       if @linkedin.update_attributes(params[:linkedin])
-        # linkedin_provider = current_user.accounts.find_by_provider('linkedin')
-        # token = linkedin_provider.oauth_token
-        # secret = linkedin_provider.oauth_token_secret
-        # verifier = linkedin_provider.oauth_verifier
-        # LinkedinWorker.perform(token,secret,verifier,@linkedin.share)
         @account = current_user.accounts.where(:provider => "linkedin").first
         time = Linkedin.where(:id => @linkedin.id).select("TIME_TO_SEC(TIME_TO_SEC(post_time)-TIME_TO_SEC(NOW())) as second").first.second
-        LinkedinWorker.perform_at(time.seconds.from_now,@account.oauth_token,@account.oauth_token_secret,@linkedin.share)
+        LinkedinWorker.perform_at(time.seconds.from_now,@account.oauth_token,@account.oauth_token_secret,@account.oauth_verifier,@linkedin.share)
         format.js{}
         format.html{ redirect_to linkedins_path}
       else
