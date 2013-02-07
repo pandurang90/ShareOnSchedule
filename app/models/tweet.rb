dcbe98e7f891a512e4655b25ad314053bdf6c884
pandurang <pandurang.plw@gmail.com>
2013-02-07 18:47:27 +0530

    callback method changes

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,8 +3,6 @@ class Tweet < ActiveRecord::Base
   validates_presence_of :content
   validates_presence_of :post_time
   belongs_to :user
-  after_save :schedule_tweet
-  after_update :schedule_tweet
 
   protected
     def schedule_tweet
