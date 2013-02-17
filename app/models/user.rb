4a87fe630da6484ebb29f004c0deb65c86c0fe6b
pandurang <pandurang.plw@gmail.com>
2013-02-17 17:43:53 +0530

    some UI changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,21 +40,21 @@ class User < ActiveRecord::Base
   def schedule_tweet(tweet)
     @tweet=tweet
     @account = self.accounts.where(:provider => "twitter").first
-    time = Tweet.where(:id => @tweet.id).select("TIME_TO_SEC(TIME_TO_SEC(post_time)-TIME_TO_SEC(NOW())) as second").first.second
+    time = @tweet.post_time-Time.now 
     TweetWorker.perform_at(time.seconds.from_now,@account.oauth_token,@account.oauth_token_secret,@tweet.id)
   end
 
   def schedule_fbpost(post)
     @fb_post=post
     @account = self.accounts.where(:provider => "facebook").first
-    time = FbPost.where(:id => @fb_post.id).select("TIME_TO_SEC(TIME_TO_SEC(post_time)-TIME_TO_SEC(NOW())) as second").first.second
+    time = @fb_post.post_time-Time.now 
     FbPostWorker.perform_at(time.seconds.from_now,@account.oauth_token,@fb_post.id)
   end
 
   def schedule_linkedin(linkedin)
     @linkedin=linkedin
     @account = self.accounts.where(:provider => "linkedin").first
-    time = Linkedin.where(:id => @linkedin.id).select("TIME_TO_SEC(TIME_TO_SEC(post_time)-TIME_TO_SEC(NOW())) as second").first.second
+    time = @linkedin.post_time-Time.now 
     LinkedinWorker.perform_at(time.seconds.from_now,@account.oauth_token,@account.oauth_token_secret,@account.oauth_verifier,@linkedin.id)
   end
 end
