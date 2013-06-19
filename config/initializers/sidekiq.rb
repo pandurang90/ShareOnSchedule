cfe8926ab1541c67fd692ef4bee03cbfeaa2b624
pandurang <pandurang.plw@gmail.com>
2013-06-19 18:41:22 +0530

    sidekiq.rb added

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,19 +1,11 @@
 require 'redis'
-if Rails.env=="development"
-  host="redis://localhost"
-  REDIS = Redis.new(host: host, port: 15001)
-elsif Rails.env == "production"
-  host = "127.3.199.129"
-  REDIS = Redis.new(host: host , port: 15001)
-end  
-
-
+REDIS = Redis.new(host: "redis://localhost", port: 15001)
  
 Sidekiq.configure_server do |config|
-  config.redis = { url: host+":15001" }
+  config.redis = { url: "redis://localhost:15001" }
 end
  
  
 Sidekiq.configure_client do |config|
-  config.redis = { url: host+":15001" }
+  config.redis = { url: "redis://localhost:15001" }
 end
